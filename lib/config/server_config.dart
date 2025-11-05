fff97b9df4e1d702c13d7d54ff2ea9f4337a747b
telkem <suu692013@gmail.com>
2025-11-05 17:51:43 +0900

    [fix] 이미지 뜨게

--- lib/config/server_config.dart
+++ lib/config/server_config.dart
@@ -1,9 +1,8 @@
 /// 서버 연결 URL 관리
 class ServerConfig {
   // 서버 기본 URL
-  static const String baseUrl = 'http://192.168.14.20:8080';
-  
+  static const String baseUrl = 'http://192.168.14.54:8080';
+
   // API 엔드포인트
   static String get apiBaseUrl => baseUrl;
 }
-
