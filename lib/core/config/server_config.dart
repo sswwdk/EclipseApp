ee4d59d1399828dea1e033496f50fff1689da795
seungwonjang <jjaga99@gmail.com>
2025-11-15 09:34:38 +0900

    Resolve merge conflict in server_config.dart

--- lib/core/config/server_config.dart
+++ lib/core/config/server_config.dart
@@ -2,14 +2,8 @@
 /// 설훈 : 54 , 석훈 : 36
 class ServerConfig {
   // 서버 기본 URL
-<<<<<<< Updated upstream
   static const String baseUrl = 'http://192.168.14.33:8080';
   static const String communityUrl = 'http://3.37.111.100:8082';
-  static const String communityUrl = 'http://192.168.14.33:8082';
-=======
-  static const String baseUrl = 'http://118.33.94.109:8080';
-  static const String communityUrl = 'http://118.33.94.109:8082';
->>>>>>> Stashed changes
 
   // API 엔드포인트
   static String get apiBaseUrl => baseUrl;
